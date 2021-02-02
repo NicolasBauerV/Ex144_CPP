7398ff8b0aa3865a73d82052c4d3ecc4e8908cc5
NicolasBauerV <bauer.nicolas266@gmail.com>
2021-02-02 17:37:40 +0100

    Initial Commit

---
+++ ex114/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ex114
+//
+//  Created by Nicolas BAUER on 02/02/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
